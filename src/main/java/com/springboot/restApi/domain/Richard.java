63568100e6c90b2c09e0f0caf8c9ea5d9697a508
CesarNicolasCabo <cesar.nicolas.cabo@gmail.com>
2020-09-26 21:04:30 -0300

    Creacion de una clase nueva

---
+++ src/main/java/com/springboot/restApi/domain/Richard.java
@@ -0,0 +1,18 @@
+package com.springboot.restApi.domain;
+
+public class Richard {
+
+    private String name;
+
+    public Richard(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
