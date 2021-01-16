9bd4cc143d1e7769d3a4d85004790f022ed6e5ae
CesarNicolasCabo <cesar.nicolas.cabo@gmail.com>
2021-01-16 18:27:25 -0300

    Adicion de headers en respuesta del controlador

--- src/main/java/com/springboot/restApi/controller/CuentaController.java
+++ src/main/java/com/springboot/restApi/controller/CuentaController.java
@@ -3,9 +3,12 @@ package com.springboot.restApi.controller;
 import com.springboot.restApi.domain.Cuenta;
 import com.springboot.restApi.service.CuentaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
@@ -20,13 +23,17 @@ public class CuentaController {
 
     @GetMapping(value="/obtener_todas_las_cuentas")
     @ResponseStatus(HttpStatus.OK)
-    public List<Cuenta> getCuentas() {
+    public List<Cuenta> getCuentas(HttpServletResponse servletResponse) {
+        servletResponse.addHeader("nombre_del_header_devuelto", "valor_del_header_devuelto");
         return cuentaService.getCuentas();
     }
 
     @RequestMapping(value = "/crear_nueva_cuenta", method = RequestMethod.POST)
-    @ResponseStatus(HttpStatus.CREATED)
-    public Cuenta addCuenta(@RequestBody Cuenta cuenta) {
-         return cuentaService.addCuenta();
+    public ResponseEntity<Cuenta> addCuenta(@RequestBody Cuenta cuenta) {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("nombre_del_header_devuelo", "valor_del_header_devuelto");
+        return ResponseEntity.status(201)
+                .header(String.valueOf(responseHeaders))
+                .body(cuentaService.addCuenta());
     }
 }
